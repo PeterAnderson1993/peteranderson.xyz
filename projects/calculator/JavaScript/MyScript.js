37ddf5a9f83f4593fa32974d57c51fe03c93f05b
PeterAnderson1993 <panderson900@gmail.com>
2023-01-22 19:33:32 +0000

    new calc functionality

--- projects/calculator/JavaScript/MyScript.js
+++ projects/calculator/JavaScript/MyScript.js
@@ -1,87 +1,29 @@
-﻿
-function no1() {
-    document.getElementById("input1").value += "1";
-}
-function no2() {
-    document.getElementById("input1").value += "2";
-}
-function no3() {
-    document.getElementById("input1").value += "3";
-}
-function no4() {
-    document.getElementById("input1").value += "4";
-}
-function no5() {
-    document.getElementById("input1").value += "5";
-}
-function no6() {
-    document.getElementById("input1").value += "6";
-}
-function no7() {
-    document.getElementById("input1").value += "7";
-}
-function no8() {
-    document.getElementById("input1").value += "8"
-}
-function no9() {
-    document.getElementById("input1").value += "9";
-}
-function no0() {
-    document.getElementById("input1").value += "0";
-}
-function point() {
-    document.getElementById("input1").value += ".";
+﻿var calculation = "";
+
+function displayCalculation() {
+  document.getElementById("sumEquals").innerHTML = calculation;
 }
 
-function divide() 
-{
-    var num1 = document.getElementById("input1").value;
-    document.getElementById("Calculation").value += num1;
-   
-    document.getElementById("Calculation").value += " / ";
-    document.getElementById("input1").value = "";;
+function addToCalculation(event) {
+  calculation += event.target.innerHTML;
+  console.log(calculation);
 }
 
-function multiply()
-{
-	var num1 = document.getElementById("input1").value;
-    document.getElementById("Calculation").value += num1;
-   
-    document.getElementById("Calculation").value += " * ";
-    document.getElementById("input1").value = "";;
+function resetConsoleCalc() {
+  console.clear();
+  calculation = "";
+  document.getElementById("sumEquals").innerHTML = "&nbsp;";
 }
 
-function minus()
-{
-	var num1 = document.getElementById("input1").value;
-    document.getElementById("Calculation").value += num1;
-   
-    document.getElementById("Calculation").value += " - ";
-    document.getElementById("input1").value = "";
+function equals() {
+  try {
+    var calculationEquals = math.evaluate(calculation);
+    console.log(calculationEquals);
+    calculation = calculationEquals;
+    document.getElementById("sumEquals").innerHTML = calculation;
+  } catch (error) {
+    console.error("Invalid Calculation");
+    document.getElementById("sumEquals").innerHTML = "Invalid";
+    calculation = "";
+  }
 }
-
-
-function plus()
-{
-    var num1 = document.getElementById("input1").value;
-    document.getElementById("Calculation").value += num1;
-   
-    document.getElementById("Calculation").value += " + ";
-    document.getElementById("input1").value = "";
-}
-
-function equals()
-{
-    var num2 = document.getElementById("input1").value;
-	
-    document.getElementById("Calculation").value += num2;
-	
-	var sum = document.getElementById("Calculation").value;
-	
-	var result = eval(sum);
-	parseInt(result);
-	document.getElementById("Calculation").value += " = ";
-	
-    document.getElementById("input1").value = result;
-}
-
